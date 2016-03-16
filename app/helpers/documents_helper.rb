41f7e4a9a9e888f785746082a1c1f0b925d8b827
Jono Mingard <reason.koan@gmail.com>
2016-03-16 16:18:24 +1300

    Allow deleting previous queries

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -11,6 +11,14 @@ module DocumentsHelper
     query.map { |term| advanced_query_term(term) }.join.html_safe
   end
 
+  def search_path_without_query(query)
+    remaining_params = request.query_parameters.except(:query)
+    query_param = params[:query].dup
+    query_param.delete({ terms: query }.to_json)
+
+    search_documents_path(remaining_params.merge(query: query_param))
+  end
+
   private
 
   def advanced_query_term(term)
