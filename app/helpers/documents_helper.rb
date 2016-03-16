38bcf82b13804819d99842598f84f7a8cb67bfdf
Jono Mingard <reason.koan@gmail.com>
2016-03-16 15:13:38 +1300

    Render advanced search queries with helpers

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -6,4 +6,18 @@ module DocumentsHelper
   def simple_text_value(query)
     query.nil? ? "" : query.first["value"]
   end
+
+  def advanced_query_view(query)
+    query.map { |term| advanced_query_term(term) }.join.html_safe
+  end
+
+  private
+
+  def advanced_query_term(term)
+    if term["operator"]
+      content_tag :span, term["operator"], class: "search-term search-term-operator #{term["operator"]}"
+    else
+      content_tag :span, term["value"], class: "search-term search-term-text"
+    end
+  end
 end
