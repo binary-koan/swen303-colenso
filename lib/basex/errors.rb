853c0b729f37e06a0f4e40f428ebe9e4d3ccb713
Jono Mingard <reason.koan@gmail.com>
2016-03-23 23:43:46 +1300

    Show a (slightly) nicer message when a search error is raised

--- lib/basex/errors.rb
+++ lib/basex/errors.rb
@@ -7,27 +7,34 @@ module BaseXClient
     end
   end
 
-  class NodeNotFound < BaseXError
+  class BXDBError < BaseXError
+  end
+
+  class XPSTError < BaseXError
+    MESSAGE = "Your query couldn't be understood."
+  end
+
+  class NodeNotFound < BXDBError
     MESSAGE = "Unable to find that document."
     ERROR_CODES = [1, 5, 6]
   end
 
-  class DatabaseNotAvailable < BaseXError
+  class DatabaseNotAvailable < BXDBError
     MESSAGE = "Cannot connect to the database."
     ERROR_CODES = [2, 3, 8, 9, 11]
   end
 
-  class InvalidIndex < BaseXError
+  class InvalidIndex < BXDBError
     MESSAGE = "The database appears to be corrupt."
     ERROR_CODES = [4]
   end
 
-  class DatabaseAlreadyOpen < BaseXError
+  class DatabaseAlreadyOpen < BXDBError
     MESSAGE = "Can't connect to the database: it is already open in another application."
     ERROR_CODES = [7]
   end
 
-  class DatabaseAlreadyExists < BaseXError
+  class DatabaseAlreadyExists < BXDBError
     MESSAGE = "Can't create the database: it already exists."
     ERROR_CODES = [12]
   end
@@ -35,11 +42,20 @@ module BaseXClient
   ERROR_CLASSES = [NodeNotFound, DatabaseNotAvailable, InvalidIndex, DatabaseAlreadyOpen, DatabaseAlreadyExists]
 
   def self.error_from(data, additional_info="")
-    error_code = /BXDB(\d{4})/.match(data).try!(:[], 1).to_i
-
-    error_class = ERROR_CLASSES.find { |klass| klass::ERROR_CODES.include?(error_code) }
-    error_class ||= BaseXError
+    if (code = error_code("BXDB", data))
+      error_class = BXDBError.subclasses.find { |klass| klass::ERROR_CODES.include?(code) }
+      error_class ||= BXDBError
+    elsif (code = error_code("XPST", data))
+      error_class = XPSTError.subclasses.find { |klass| klass::ERROR_CODES.include?(code) }
+      error_class ||= XPSTError
+    end
 
     error_class.new("#{data}\n#{additional_info}")
+  end
+
+  def self.error_code(type, data)
+    code = /#{type}(\d{4})/.match(data).try!(:[], 1).to_i
+
+    code > 0 ? code : nil
   end
 end
