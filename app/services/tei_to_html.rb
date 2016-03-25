1749d08f5dffc3fafad8fc6f7033d606bc7ab494
Jono Mingard <reason.koan@gmail.com>
2016-03-25 19:08:33 +1300

    Improve display of TEI documents - fixes #32
    Convert more TEI tags and render methods to HTML; improve header
    display on document show and edit pages.

--- app/services/tei_to_html.rb
+++ app/services/tei_to_html.rb
@@ -14,18 +14,24 @@ class TeiToHtml
   private
 
   def convert_node(node)
-    return "" if node.nil?
+    return if node.nil? || node.comment?
     return node.text if node.text?
 
     case node.name
-    when "front" then build_tag(node, "section", class: "front")
-    when "body"  then build_tag(node, "section", class: "body")
+    when "front" then build_tag(node, "section", class: "tei-front")
+    when "body"  then build_tag(node, "section", class: "tei-body")
+    when "div"   then build_tag(node, "div")
     when "p"     then build_tag(node, "p")
     when "note"  then build_note(node)
-    when "hi"    then build_tag(node, "span")
     when "table" then build_tag(node, "table", class: "table")
     when "row"   then build_tag(node, "tr")
     when "cell"  then build_tag(node, "td")
+    when "lb"    then build_tag(node, "br")
+    when "list"  then build_list(node)
+    when "item"  then build_tag(node, "li")
+    when "anchor", "ptr", "hi", "date"
+      # Hard to see how these could be translated meaningfully into HTML
+      build_tag(node, "span")
     else
       puts "WARNING: Unknown tag #{node.name}"
       build_tag(node, "span")
@@ -37,12 +43,20 @@ class TeiToHtml
   end
 
   def build_tag(node, name, attrs={})
-    attrs[:class] = attrs[:class].present? ? "#{attrs[:class]} #{html_class(node)}" : html_class(node)
+    name, attrs = transform_with_render_method(node, name, attrs)
 
     if node.children.present?
       content_tag(name, convert_children(node), attrs)
     else
       tag(name, attrs)
+    end
+  end
+
+  def build_list(node)
+    if node["type"] == "ordered"
+      build_tag(node, "ol")
+    else
+      build_tag(node, "ul")
     end
   end
 
@@ -55,18 +69,32 @@ class TeiToHtml
     )
   end
 
-  def html_class(node)
+  def transform_with_render_method(node, name, attrs)
     render_method = node["rend"]
-    return unless render_method
+    return [name, attrs] unless render_method
 
     render_method.split(/\s+/).map do |method|
       case method
-      when "bold"      then "text-bold"
-      when "italic"    then "text-italic"
-      when "smallcaps" then "text-smallcaps"
-      when "center"    then "text-center"
-      else puts "WARNING: Unknown render method #{render_method}"
+      when "bold"            then add_class(attrs, "tei-bold")
+      when "italic"          then add_class(attrs, "tei-italic")
+      when "underline"       then add_class(attrs, "tei-underline")
+      when "doubleunderline" then add_class(attrs, "tei-doubleunderline")
+      when "strikethrough"   then add_class(attrs, "tei-strikethrough")
+      when "smallcaps"       then add_class(attrs, "tei-smallcaps")
+      when "sup", "sub"      then name = method
+      when "center"          then add_class(attrs, "tei-center")
+      when "justify"         then add_class(attrs, "tei-justify")
+      when "frame"           then add_class(attrs, "tei-frame")
+      when "end"             # ignore
+      else puts "WARNING: Unknown render method #{method}"
       end
     end.compact.join(" ")
+
+    [name, attrs]
+  end
+
+  def add_class(attrs, class_name)
+    attrs[:class] ||= ""
+    attrs[:class] += " #{class_name}"
   end
 end
